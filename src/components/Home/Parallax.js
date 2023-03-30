4694e29c102eea7ca3badbd6b187c37694e25b86
Ilan Oliveira Costa <79155978+Ilan-dev0@users.noreply.github.com>
2023-03-30 16:57:02 -0300

    finalizado

--- src/components/Home/Parallax.js
+++ src/components/Home/Parallax.js
@@ -13,7 +13,7 @@ const Parallax = () => {
         <h2 className='text-zinc-900 text-center text-2xl md:text-4xl text-bold '>NÓS VENDEMOS SEGURANÇA</h2>
           <p className='text-zinc-800 text-1xl italic text-center  pt-5'>Para tirar dúvidas ou fazer um orçamento sem compromisso <br></br> entre em contato com a nossa equipe agora mesmo.</p>
           <div className='md:px-10 md:ml-64 md:mb-20 mb-20   px-14 justify-items-center sm:flex sm:justify-items-start'>
-            <button onClick={handleClick} className=' sm:ml-60 rounded-none px-16 py-4 mt-8 text-red-900 bg-transparent hover:bg-red-800 hover:border-red-800 hover:text-white border-red-99'>QUERO FALAR COM A VERONA!</button>
+            <button onClick={handleClick} className='  rounded-none px-10 md:px-16 py-4 mt-8 text-red-900 bg-transparent hover:bg-red-800 hover:border-red-800 hover:text-white border-red-99'>QUERO FALAR COM A VERONA!</button>
           </div>
       </div>
     </div>
